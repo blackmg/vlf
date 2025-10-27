5c74a923ad7b4f2a70d186fa4df84224b24eb7c2
Johan Öhman <johan.s.ohman@gmail.com>
2025-10-27 12:54:27 +0100

    WiP

--- slf4j/build.gradle.kts
+++ slf4j/build.gradle.kts
@@ -6,10 +6,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 //}
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
-//group = "io.backvision.vlf"
-//version = "unspecified"
+group = "io.backvision.vlf"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -22,6 +23,10 @@ repositories {
 //tasks.test {
 //    useJUnitPlatform()
 //}
+val sourcesJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.map { it.allSource.sourceDirectories })
+}
 
 tasks.withType<KotlinCompile>().all {
     compilerOptions {
@@ -45,4 +50,41 @@ dependencies {
 kotlin {
 //    jvm()
     jvmToolchain(17)
+}
+
+//val javadocJar = tasks.getByName("javadocJar")
+//val sourcesJar = tasks.getByName("sourcesJar")
+
+
+configure<PublishingExtension> {
+    publications {
+        register<MavenPublication>("java") {
+            artifact(sourcesJar)
+//            artifact(javadocJar)
+            afterEvaluate {
+                from(components["java"])
+            }
+            pom {
+                name.set("Slf4j Vlf processor")
+//                description.set("KOIN - Kotlin simple Dependency Injection Framework")
+//                url.set("https://insert-koin.io/")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+//                    url.set("https://github.com/InsertKoinIO/koin")
+//                    connection.set("scm:git:https://github.com/InsertKoinIO/koin.git")
+                }
+                developers {
+                    developer {
+                        name.set("Johan Öhman")
+//                        email.set("arnaud@kotzilla.io")
+                    }
+                }
+            }
+        }
+    }
 }
