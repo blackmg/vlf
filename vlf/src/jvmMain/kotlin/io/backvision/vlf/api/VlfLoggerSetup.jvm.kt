24ba8c06f74db0798ef5436af663d326e13cc958
Johan Öhman <johan.s.ohman@gmail.com>
2025-10-29 13:03:36 +0100

    WiP

--- vlf/src/jvmMain/kotlin/io/backvision/vlf/api/VlfLoggerSetup.jvm.kt
+++ vlf/src/jvmMain/kotlin/io/backvision/vlf/api/VlfLoggerSetup.jvm.kt
@@ -2,9 +2,9 @@ package io.backvision.vlf.api
 
 import io.backvision.vlf.impl.PrintLoggerProcessor
 
-actual val platformServiceLocator: VlfServiceLocator = VlfServiceLocator { load() }
+actual val platformServiceLocator: VlfServiceLocator = VlfServiceLocator {
+    (Class.forName("io.backvision.vlf.jvm.VlfServiceLocatorImpl")
+        .getDeclaredConstructor().newInstance() as? VlfServiceLocator)?.loadPlatformDefault() ?: PrintLoggerProcessor()
+}
 
-
-fun load(): VlfLogProcessor = (Class.forName("io.backvision.vlf.jvm.VlfServiceLocatorImpl")
-    .newInstance() as? VlfServiceLocator)?.loadPlatformDefault() ?: PrintLoggerProcessor()
 
